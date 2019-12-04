8030b37cd3f24804e6c81b641055ee4d09d0fcee
Chipotle coders <savir@chipotie.io>
2019-12-04 00:01:38 -0600

    Final refactor

--- app/src/androidTest/java/io/pulque/blindfold/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/io/pulque/blindfold/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("io.chipotie.blindfold", appContext.packageName)
+        assertEquals("io.pulque.blindhorse", appContext.packageName)
     }
 }
--- fastchoicebutton/src/androidTest/java/io/pulque/fastchoicebutton/ExampleInstrumentedTest.kt
+++ fastchoicebutton/src/androidTest/java/io/pulque/fastchoicebutton/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("io.chipotie.fastchoicebutton.test", appContext.packageName)
+        assertEquals("io.pulque.fastchoicebutton.test", appContext.packageName)
     }
 }
